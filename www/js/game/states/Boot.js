1af25c184e1a8b87d4061253d81bc2196038f646
NoSinging <NoSinging@users.noreply.github.com>
2016-08-22 22:23:25 +1000

    Add various ipad, iphone store images Update game size to work with iphone 4

--- www/js/game/states/Boot.js
+++ www/js/game/states/Boot.js
@@ -17,22 +17,12 @@ angryMaths.Boot.prototype = {
     //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
     this.input.maxPointers = 1;
 
-    if (this.game.device.desktop) {
-      //  If you have any desktop specific settings, they can go in here
-      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-      game.scale.setMinMax(480, 260, 2048, 2732);
-      game.scale.pageAlignHorizontally = true;
-      game.scale.pageAlignVertically = true;
-      this.scale.refresh();
-    } else {
-      //  Same goes for mobile settings.
-      //  scale the game, no lower than low res and no higher than ipad pro (2048, 2732)
-      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-      game.scale.setMinMax(320, 568, 2048, 2732);
-      game.scale.pageAlignHorizontally = true;
-      game.scale.pageAlignVertically = true;
-      this.scale.refresh();
-    }
+    //  scale the game, no lower than iphone 4 (320,480) and no higher than ipad pro (2048, 2732)
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.setMinMax(320, 480, 2048, 2732);
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    this.scale.refresh();
 
     //  By this point the preloader assets have loaded to the cache, we've set the game settings
     //  So now let's start the real preloader going
